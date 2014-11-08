886ee1bfb0b53108dba1bc25c682009780dfee69
sunsun <fengyingandong@163.com>
2014-11-08 15:50:55 +0800

    第二次 Signed-off-by: sunsun <fengyingandong@163.com>

---
+++ src/com/sunsun/framework/component/base/IEntity.java
@@ -0,0 +1,13 @@
+package com.sunsun.framework.component.base;
+
+import java.io.Serializable;
+
+/**
+ * 描述:实体类接口，简单继承Serializable
+ * 
+ * @author chenys
+ * @since 2013-7-29 下午2:11:11
+ */
+public interface IEntity extends Serializable {
+
+}
